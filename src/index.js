e5a74266be1573cdcbc01f3ebe1947e11609b512
afikanzoro <muchemimutahi@gmail.com>
2022-07-12 18:17:37 +0300

    Make changes as requesed by the reviewer

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter store={store}>
-      <Provider>
+    <BrowserRouter>
+      <Provider store={store}>
         <App />
       </Provider>
     </BrowserRouter>
